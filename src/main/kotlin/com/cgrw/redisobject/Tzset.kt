d35e066eb89ced3b488a3c1394ac369cc0f4facb
cgIIrw <chenguo_jay@163.com>
2019-03-10 11:59:28 +0800

    添加用来创建跳跃表节点对象和跳跃表对象的函数

--- src/main/kotlin/com/cgrw/redisobject/Tzset.kt
+++ src/main/kotlin/com/cgrw/redisobject/Tzset.kt
@@ -1,6 +1,44 @@
 package com.cgrw.redisobject
 
+import com.cgrw.structure.ZskipListNode
+import com.cgrw.structure.Zskiplist
+
 /**
  * @Author: cgrw
  * @Url: https://github.com/cgIIrw/kodis
  */
+const val ZSKIPLIST_MAXLEVEL = 32
+
+
+// 创建并返回单个跳跃表节点
+fun zslCreateNode(level: Int, score: Double, o: Any?): ZskipListNode {
+    var zn = ZskipListNode()
+    zn.level = arrayOfNulls(level)
+    zn.score = score
+    zn.obj = o
+    return zn
+}
+
+// 创建跳跃表，初始化头节点
+fun zslCreate(): Zskiplist {
+    // 初始化一个跳跃表实例
+    var zsl = Zskiplist()
+    // 最大节点层数
+    zsl.level = 1
+    // 表中节点数量，不包括头节点
+    zsl.length = 0
+
+    // 初始化头节点
+    zsl.header = zslCreateNode(ZSKIPLIST_MAXLEVEL, (0).toDouble(), null)
+
+    for (i in 0 until ZSKIPLIST_MAXLEVEL) {
+        zsl.header.level[i]!!.forward = null
+        zsl.header.level[i]!!.span = 0
+    }
+    zsl.header.backward = null
+    zsl.tail = null
+    return zsl
+}
+
+
+
