363863c3de9d970ceaabe86cbe3e02ef4304b0c4
cgIIrw <chenguo_jay@163.com>
2019-04-09 22:18:22 +0800

    添加双端链表类

--- src/main/kotlin/com/cgrw/structure/Adlist.kt
+++ src/main/kotlin/com/cgrw/structure/Adlist.kt
@@ -3,4 +3,19 @@ package com.cgrw.structure
 /**
  * @Author: cgrw
  * @Url: https://github.com/cgIIrw/kodis
+ * @Desc: 实现双端链表
  */
+
+// 链表节点
+class ListNode {
+    var prev: ListNode? = null
+    var next: ListNode? = null
+    var value: Any? = null
+}
+
+class List {
+    var head: ListNode? = null
+    var tail: ListNode? = null
+    var len: Long? = null
+    // todo 缺失一些未实现的方法
+}
