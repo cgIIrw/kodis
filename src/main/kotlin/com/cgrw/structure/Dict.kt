91c0772a65696f93ea52efa1463cba95f22b3151
cgIIrw <chenguo_jay@163.com>
2019-04-09 22:23:52 +0800

    给字典添加hash等方法

--- src/main/kotlin/com/cgrw/structure/Dict.kt
+++ src/main/kotlin/com/cgrw/structure/Dict.kt
@@ -6,6 +6,9 @@ package com.cgrw.structure
  * @Desc: 实现字典
  */
 
+val DICT_OK = 0
+val DICT_ERR = 1
+
 class Dict {
 
     // dictType 处理特定类型的函数
@@ -14,11 +17,17 @@ class Dict {
     //  类型处理函数私有数据
     var privdata: Any? = null
 
-    var ht: Array<Array<DictEntry>?> = arrayOfNulls(2)
+    var ht: Array<Dictht?> = arrayOfNulls(2)
 
-    var rehashidx: Int? = null
+    var rehashidx: Int = 0
 
-    var iterators: Int? = null
+    var iterators: Int = 0
+}
+
+class Dictht {
+    var table: Array<DictEntry>? = null
+    var size: Int = 0
+    var used: Int = 0
 }
 
 class MethodsType {
@@ -29,4 +38,20 @@ class DictEntry {
     var key: Any? = null
     var value: Any? = null
     var next: DictEntry? = null
+}
+
+// todo 使用了Java 8的hash方法，后期替换完善
+fun hash(key: Any?): Int {
+    return key.hashCode() xor (key.hashCode() ushr 16)
+}
+
+fun _dictInit(ht: Dict, privDataPtr: Any?): Int {
+    ht.privdata = privDataPtr
+    return DICT_OK
+}
+
+fun dictCreate(privdata: Any?): Dict {
+    var ht = Dict()
+    _dictInit(ht, privdata)
+    return ht
 }
